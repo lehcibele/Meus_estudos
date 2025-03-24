ec30380368673aa4900ac53a778766aa2ac93cc7
Leticia Cibele <leticiacibele.cl@gmail.com>
2025-03-24 10:35:25 -0300

    comentarios, print e string
    assuntos estudados da aula 1 até a 3

---
+++ python/aula1.py
@@ -0,0 +1,12 @@
+"""
+    DoString --> O interpretador ler isso, mas não interpreta como código.
+    Isso é um comentário em bloco
+"""
+'''
+    Outra forma de comentário em bloco
+'''
+
+# Permite escrever um comentário
+# função print exibe alguma coisa na tela (output)
+print(123) # Na frente
+# Abaixo
---
+++ python/aula2.py
@@ -0,0 +1,9 @@
+# função para exibir coisas na tela, a função recebe argumentos.
+print(12, 34) # os argumentos são separados por virgula (argumentos não nomeados são separados por virgula).
+
+print(56, 78, sep='-') # Utilizando outro separador com sep (pode ser qualquer coisa).
+
+print(9, 10, sep='-', end='\n##') # final do print, depois do ultimo argumento.
+
+# Quebra de linha \n
+print(1,'\n',2)
