cc82bbf03d2873db8424d3c1df274b8db7d1cea2
Leticia Cibele <leticiacibele.cl@gmail.com>
2025-04-11 18:59:09 -0300

    estudos python

---
+++ python/14-operadores-relacionais.py
@@ -0,0 +1,18 @@
+"""
+    Operadores de comparação (Relacionais)
+    OPERADOR    SIGNIFICADO    EXEMPLO(TRUE)
+        >          maior           2 > 1
+        >=       maior ou igual    2 >= 1
+        <          menor           1 < 2
+        <=      menor ou igual    1 <= 2
+        ==         igual           'a' == 'a'
+        !=         diferente       'a' != 'b'
+"""
+
+maior = 2 > 1
+maior_ou_igual = 2 >= 1
+menor = 1 < 2
+menor_ou_igual = 1 <= 2
+igual = 'a' == 'a'
+diferente = 'a' != 'b'
+print(diferente)  # True
