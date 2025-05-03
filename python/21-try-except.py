1b440d98649a111fafbcbdf70a6d2a835241bfad
Leticia Cibele <leticiacibele.cl@gmail.com>
2025-05-03 13:15:49 -0300

    try e except

--- python/21-try-except.py
+++ python/21-try-except.py
@@ -7,7 +7,9 @@ numero_str = input(
 ) # input() -> sempre retorna string
 
 try:
+    print('STR: ', numero_str)
     numero_float = float(numero_str)
+    print('FLOAT: ', numero_float)
     print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
 except:
     print('Isso não é um número')
