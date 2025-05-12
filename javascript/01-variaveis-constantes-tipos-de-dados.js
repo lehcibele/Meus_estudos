da8628d9011c69a0056709e1396a3b4a5f932345
Leticia Cibele <leticiacibele.cl@gmail.com>
2025-05-12 20:27:48 -0300

    tipos de dados em js

---
+++ javascript/01-variaveis-constantes-tipos-de-dados.js
@@ -0,0 +1,15 @@
+// let -> define que é uma variável
+
+let altura = 1.70;
+let peso = 70;
+
+let imc = peso / (altura * altura);
+
+console.log(imc);
+
+// Tipos de dados
+
+let nome = "Leticia";               // Strings
+let numero = 1200;                  // Inteiros 
+let decimal = 1.25;                 // decimal
+let avancarProximoEpisodio = false  // booleano
