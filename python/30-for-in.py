d14f2417338d24de40295e7644a05cfc38f347eb
Leticia Cibele <leticiacibele.cl@gmail.com>
2025-05-12 21:11:12 -0300

    for/in e range

---
+++ python/30-for-in.py
@@ -0,0 +1,10 @@
+# Estrutura de repetição para coisas finitas
+
+texto = 'Python'
+novo_texto = ''
+
+for letra in texto:
+    novo_texto += f'*{letra}'
+    print(letra)
+
+print(novo_texto)
---
+++ python/31-range-for.py
@@ -0,0 +1,12 @@
+"""
+    For + Range
+    range -> range(start, top, step)
+"""
+
+# numeros = range(10) # numeros de 0 até 9
+# numeros = range(5, 10) # começa em 5 e vai até 9
+numeros = range(5, 10, 2) # começa em 5 e vai pulando de 2 em 2 até 9
+
+for numero in numeros:
+    print(numero)
+
