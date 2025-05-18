2e57065ffbefb8a9b6bb2bda568493fc918baec1
Leticia Cibele <leticiacibele.cl@gmail.com>
2025-05-18 11:34:47 -0300

    estudando funções anonimas e atualizando o progresso do curso de front para 40% concluido

---
+++ javascript/07-funcoes-anonimas-arrow.js
@@ -0,0 +1,25 @@
+// Função literal
+// function soma(a, b){
+//     console.log(a+b);
+// }
+
+// soma(1, 2);
+
+// Função anônima (sem nome)
+// const somar = function (a, b) {
+//     console.log(a+b);
+// }
+
+// somar(1, 2);
+
+// Função arrow (seta)
+// const somar = (a, b) => {
+//     console.log(a+b);
+// }
+
+// somar(1, 2);
+
+// Função arrow (retorno implicito)
+const somar = (a, b) => console.log(a+b);
+
+somar(1, 2);
