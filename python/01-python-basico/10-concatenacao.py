bc33be7054115e8229645a20db4eadf5efc41d25
Leticia Cibele <leticiacibele.cl@gmail.com>
2025-05-18 10:09:17 -0300

    excluindo arquivos py

--- python/01-python-basico/10-concatenacao.py
+++
@@ -1,8 +0,0 @@
-concatenacao = 'A' + 'B' + 'C'
-print(concatenacao)
-
-# repetição com *
-a_dez_vezes = 'A' * 10 # vai imprimir A dez vezes
-tres_vezes_leticia = 3 * 'Leticia' # vai imprimir 3 vezes Leticia
-print(a_dez_vezes)
-print(tres_vezes_leticia)
--- python/01-python-basico/16-in-e-not.py
+++
@@ -1,9 +0,0 @@
-# Operadores in e not in
-# Strings são iteráveis
-nome = 'Leticia'
-print('t' in nome) # True
-print('d' in nome) # False
-print('et' in nome) # True
-print('a' not in nome) # False, a está entre o nome
-print('va' not in nome) # True, va não está entre o nome
-
