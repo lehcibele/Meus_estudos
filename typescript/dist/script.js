2c94047f92dec1d9fc4f9e35040d99c1ebf3650a
Leticia Cibele <leticiacibele.cl@gmail.com>
2025-06-19 19:27:22 -0300

    deletando arquivos da pasta dist

--- typescript/dist/script.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sum = sum;
-function sum(a, b) {
-    console.log('Dentro escorpo de módulo');
-    return { result: a + b };
-}
