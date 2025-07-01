493358e7f7de79d9840361740c67cced59d5369f
Leticia Cibele <leticiacibele.cl@gmail.com>
2025-07-01 10:00:23 -0300

    Create 15-venv.py

---
+++ python/02-python-intermediario/15-venv.py
@@ -0,0 +1,18 @@
+"""
+    Ambientes virtuais em python (venv)
+    Um ambiente virtual carrega toda a sua instalação do python para uma pasta no caminho escolhido. 
+    Ao ativar um ambiente virtual, a instalação do ambiente virtual será usada. venv é o modulo que vamos usar para criar ambiente virtuais.
+    Você pode dar o nome que preferir para um ambiente virtual, mas os mais comuns são: 
+        venv env .venv .env
+"""
+
+"""
+    Cria o ambiente virtual:
+        python -m venv venv # (nome do ambiente virtual)
+
+    Ativar o ambiente virtual:
+        venv\scripts\activate
+    
+    Para desativar o ambiente virtual:
+        deactivate
+"""
