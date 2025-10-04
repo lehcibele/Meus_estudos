8162f0cc509678c70ac058f6a24beb87ff21ee0d
Leticia Cibele <leticiacibele.cl@gmail.com>
2025-10-04 12:49:28 -0300

    continuando os estudos em react

---
+++ react-fundamentos/src/pages/_app.tsx
@@ -0,0 +1,7 @@
+export default function App(props: any) {
+    console.log(props)
+    
+    const { Component, pageProps } = props;
+
+    return <Component {...pageProps} />
+}
--- react-fundamentos/src/pages/blog/index.tsx
+++ react-fundamentos/src/pages/blog/index.tsx
@@ -1,4 +1,7 @@
 // Usar pages é a maneira antiga de criar rotas, atualmente usamos a pasta src -> app -> e dentro de app fica as rotas (páginas), desse modo nem todos os arquivos de app vão ser rotas, já usando page todos os arquivos viram rotas
+
+import '../../app/globals.css'
+
 export default function Page() {
-    return <h1>Blog</h1>
+    return <h1 className="bg-red-500">Blog</h1>
 }
