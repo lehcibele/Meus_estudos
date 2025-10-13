2712bb00a7fe6882ed10be995c93b669dfcca6c5
Leticia Cibele <leticiacibele.cl@gmail.com>
2025-10-13 13:37:48 -0300

    estudos react

--- react-fundamentos/src/app/fundamentos/7_entrada_de_dados/page.tsx
+++ react-fundamentos/src/app/fundamentos/7_entrada_de_dados/page.tsx
@@ -1,26 +1,26 @@
-'use client'
-// import { useState } from "react"
+// 'use client'
+// // import { useState } from "react"
 
-export default function Entrada () {
+// export default function Entrada () {
 
-    // const [valor, setValor] = useState("Inicial");
+//     // const [valor, setValor] = useState("Inicial");
 
-    // function alterarValor(e) {
-    //     setValor(e.target.value)
-    // }
+//     // function alterarValor(e) {
+//     //     setValor(e.target.value)
+//     // }
 
-    return (
-        <div className="p-2">
-            <h1>Entrada de dados</h1>
-            <input 
-            className="campo-texto"
-            type="text"
-            value={valor} 
-            // onChange={e => setValor(e.target.value)} - captura o valor digitado pelo usuario
-            // onChange={alterarValor}
-            />
+//     return (
+//         <div className="p-2">
+//             <h1>Entrada de dados</h1>
+//             <input 
+//             className="campo-texto"
+//             type="text"
+//             value={valor} 
+//             // onChange={e => setValor(e.target.value)} - captura o valor digitado pelo usuario
+//             // onChange={alterarValor}
+//             />
 
-            <h4>Valor digitado: {valor}</h4>
-        </div>
-    )
-}
+//             <h4>Valor digitado: {valor}</h4>
+//         </div>
+//     )
+// }
